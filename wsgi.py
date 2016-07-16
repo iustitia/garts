171b5eaf9610683204dc0b7b67eb4120e90adda8
iustitia <jkubisztal@gmail.com>
2016-07-16 11:33:54 +0200

    cleaning

--- wsgi.py
+++ wsgi.py
@@ -3,9 +3,7 @@
 # To use your own django app use code like this:
 import os
 import sys
-#
-## assuming your django settings file is at '/home/garts/mysite/mysite/settings.py'
-## and your manage.py is is at '/home/garts/mysite/manage.py'
+
 path = '/home/garts/garts/'
 if path not in sys.path:
     sys.path.append(path)
