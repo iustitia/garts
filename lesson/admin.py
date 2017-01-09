01d47bdecfc2cc3517b1bfb9fb4a4e587cbfa3cb
iustitia <jkubisztal@gmail.com>
2017-01-09 16:53:06 +0100

    overriden article question, ArticleQuestion added to Admin panel

--- lesson/admin.py
+++ lesson/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import ArticleQuestion, Word
+
+
+class ArticleQuestionAdmin(admin.ModelAdmin):
+    list_display = ('content', 'category', )
+    list_filter = ('category', 'related_word_id')
+    fields = ('content', 'category', 'sub_category',
+              'figure', 'quiz', 'explanation',)
+
+    search_fields = ('content', 'explanation')
+    filter_horizontal = ('quiz',)
+
+
+class WordAdmin(admin.ModelAdmin):
+    fields = ('name', 'article', 'meaning', 'part_of_speech',
+              'figure', 'extra',)
+
 
 # Register your models here.
+admin.site.register(ArticleQuestion, ArticleQuestionAdmin)
+admin.site.register(Word, WordAdmin)
--- lesson/models.py
+++ lesson/models.py
@@ -1,4 +1,57 @@
 from django.db import models
-from wordbrowser.models import Word
+from django.utils.translation import ugettext as _
+from true_false.models import TF_Question
 
 
+class Word(models.Model):
+    ARTICLES = (('der', 'der'), ('die', 'die'), ('das', 'das'))
+    name = models.CharField(max_length=150)
+    article = models.CharField(max_length=3, choices=ARTICLES)
+    part_of_speech = models.CharField(max_length=50, blank=True)
+    meaning = models.CharField(max_length=100, blank=True)
+    extra = models.CharField(max_length=100, blank=True)
+    figure = models.ImageField(upload_to='uploads/%Y/%m/%d',
+                               blank=True,
+                               null=True,
+                               verbose_name=_("Figure"))
+
+    def save(self, *args, **kwargs):
+        is_new = not self.id
+        super(Word, self).save(*args, *kwargs)
+        if is_new:
+            art_question = ArticleQuestion(related_word_id=self, content=self.name, article=self.article, figure=self.figure)
+            art_question.save()
+
+    def __str__(self):
+        return self.name
+
+
+class ArticleQuestion(TF_Question):
+
+    article = models.CharField(blank=False,
+                               default=False,
+                               max_length=3)
+
+    related_word_id = models.ForeignKey(Word, verbose_name=_("Question"))
+
+    def check_if_correct(self, guess):
+        if guess == self.article:
+            return True
+        else:
+            return False
+
+    def get_answers(self):
+
+        return[{'correct': self.check_if_correct("der"),
+                'content': 'der'},
+               {'correct': self.check_if_correct("die"),
+                'content': 'die'},
+               {'correct': self.check_if_correct("das"),
+                'content': 'das'
+                }]
+
+    def get_answers_list(self):
+        return Word.ARTICLES
+
+    def answer_choice_to_string(self, guess):
+        return str(guess)
