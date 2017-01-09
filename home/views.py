4f46e0f1c914d13f511b894927c64e2f9c7dd576
iustitia <jkubisztal@gmail.com>
2017-01-09 16:49:18 +0100

    clear unused code

--- home/views.py
+++ home/views.py
@@ -3,10 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def index2(request):
-
-    return HttpResponse("Hello world, main page works.")
-
 
 def index(request):
 
