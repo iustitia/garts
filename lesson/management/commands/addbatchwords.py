e405ca06dee6f80ee0b7c21ce3209b00aa1d88d8
iustitia <jkubisztal@gmail.com>
2017-01-18 11:18:11 +0100

    migrations

---
+++ lesson/management/commands/addbatchwords.py
@@ -0,0 +1,26 @@
+import os
+from django.core.management.base import BaseCommand
+from lesson.models import Word
+
+
+class Command(BaseCommand):
+    help = "Add batch words from specified file."
+
+    def handle(self, *args, **options):
+        sourcefile = '/home/iustitia/workspace/random/data/top_articled.txt'
+
+        with open(sourcefile, 'r') as f:
+            content = f.readlines()
+
+            for nr, line in enumerate(content):
+
+                l = str.split(line, '] [')
+
+                a = str.strip(l[0], '[\'').split()
+                w = a[0].strip(',').replace('\\xad', '')
+                article = a[1].strip(',')
+
+                rank = line.count('▮')
+
+                new_word = Word(name=w, article=article, rank_in_tops=nr, source='tatoeba', rank_in_dict=rank)
+                new_word.save()
