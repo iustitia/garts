aaddc822f100a1458cc86f09a8a34af01e9bb1dc
iustitia <jkubisztal@gmail.com>
2017-01-18 18:23:31 +0100

    db settings

--- garts/setts/pyany.py
+++ garts/setts/pyany.py
@@ -7,7 +7,7 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db',
+        'NAME': '/home/germanarticles/garts/db',
         'USER': 'iustitia'
     }
 }
