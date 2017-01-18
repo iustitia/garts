df1273b87b75fb787b98dbdf746003b0f686fd89
iustitia <jkubisztal@gmail.com>
2017-01-18 10:51:26 +0100

    wordbrowser edit

---
+++ garts/setts/pyany.py
@@ -0,0 +1,18 @@
+import yaml
+
+STATIC_ROOT = '/static/'
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db',
+        'USER': 'iustitia'
+    }
+}
+
+with open('garts/setts/key.yaml', 'r') as y:
+    doc = yaml.load(y)
+
+SECRET_KEY = doc['key']
