b9bfafed52d2c1f014892279dd9062915c799c35
iustitia <jkubisztal@gmail.com>
2017-01-31 15:09:00 +0100

    result page UI update

--- garts/settings.py
+++ garts/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'garts.wsgi.application'
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
@@ -90,5 +90,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 LOGIN_REDIRECT_URL = '/'
