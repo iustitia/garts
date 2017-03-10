288b2b0e8473f04c6b53be7bc3cc86a50bd061d9
iustitia <jkubisztal@gmail.com>
2017-03-10 13:49:22 +0100

    user registration

--- garts/urls.py
+++ garts/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from home import views
 from django.contrib.auth import views as auth_views
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 from quiz.views import QuizListView, QuizUserProgressView
 from lesson.views import QuizView
 
@@ -42,5 +44,11 @@ urlpatterns = [
     url(r'', include('lesson.urls')),
     url(r'^quiz/', include(quiz_urls)),
     url(r'^login', auth_views.login, name='login'),
-    url(r'^logout', auth_views.logout, name='logout', kwargs={'next_page': '/'})
+    url(r'^logout', auth_views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^register/', CreateView.as_view(
+        template_name='register.html',
+        form_class=UserCreationForm,
+        success_url='/login'
+    )),
+    url(r'^accounts/', include('django.contrib.auth.urls'))
 ]
