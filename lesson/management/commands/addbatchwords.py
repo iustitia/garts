50c298abc20cf313f47a8c2bfd0b98460fb7ff87
iustitia <jkubisztal@gmail.com>
2017-04-14 13:35:59 +0200

    batch add

--- lesson/management/commands/addbatchwords.py
+++ lesson/management/commands/addbatchwords.py
@@ -14,18 +14,17 @@ class Command(BaseCommand):
         print(options)
         sourcefile = options['path']
 
-        with open(sourcefile, 'r') as f:
-            content = f.readlines()
+        content = sourcefile.readlines()
 
-            for nr, line in enumerate(content):
+        for nr, line in enumerate(content):
 
-                l = str.split(line, '] [')
+            l = str.split(line, '] [')
 
-                a = str.strip(l[0], '[\'').split()
-                w = a[0].strip(',').replace('\\xad', '')
-                article = a[1].strip(',')
+            a = str.strip(l[0], '[\'').split()
+            w = a[0].strip(',').replace('\\xad', '')
+            article = a[1].strip(',')
 
-                rank = line.count('▮')
+            rank = line.count('▮')
 
-                new_word = Word(name=w, article=article, rank_in_tops=nr, source='tatoeba', rank_in_dict=rank)
-                new_word.save()
+            new_word = Word(name=w, article=article, rank_in_tops=nr, source='tatoeba', rank_in_dict=rank)
+            new_word.save()
