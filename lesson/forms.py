13f3b8783ecf719579fc875e942e5f8f3d04f416
iustitia <jkubisztal@gmail.com>
2017-04-18 19:21:17 +0200

    native lang for registration set

--- lesson/forms.py
+++ lesson/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.conf import settings
+from django.conf.global_settings import LANGUAGES
 
-choices = (('A1', 'A1'),
+choices = (('', '-------------'),
+           ('A1', 'A1'),
             ('A2', 'A2'),
             ('B1', 'B1'),
             ('B2', 'B2'),
@@ -14,7 +15,7 @@ choices = (('A1', 'A1'),
 
 class RegistrationForm(UserCreationForm):
     languagelevel = forms.ChoiceField(choices=choices, label="German language level", help_text='Language level according to CEFR ->')
-    native = forms.ChoiceField(choices=settings.LANGUAGES, label="Native language")
+    native = forms.ChoiceField(choices=(('', '-------------'),) + tuple(LANGUAGES), label="Native language")
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
     source = forms.CharField(max_length=254, required=False, label="Where have you heard about this site?")
 
